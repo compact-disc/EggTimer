bbf4d2e5fc5ff22645f1d43b7b38ee9e94fbf6ab
Christopher DeRoche <46451756+compact-disc@users.noreply.github.com>
2021-09-01 23:53:15 -0500

    Added egg timer files

---
+++ EggTimer/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace EggTimer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.minutesLabel = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.secondsUpDown = new System.Windows.Forms.NumericUpDown();
+            this.minutesUpDown = new System.Windows.Forms.NumericUpDown();
+            this.hoursUpDown = new System.Windows.Forms.NumericUpDown();
+            this.countdownTimer = new System.Windows.Forms.Timer(this.components);
+            this.restartButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.Color.White;
+            this.titleLabel.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(104, 19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(177, 45);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Egg Timer";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLabel.Location = new System.Drawing.Point(204, 79);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(86, 27);
+            this.secondsLabel.TabIndex = 2;
+            this.secondsLabel.Text = "Seconds";
+            // 
+            // minutesLabel
+            // 
+            this.minutesLabel.AutoSize = true;
+            this.minutesLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesLabel.Location = new System.Drawing.Point(204, 115);
+            this.minutesLabel.Name = "minutesLabel";
+            this.minutesLabel.Size = new System.Drawing.Size(83, 27);
+            this.minutesLabel.TabIndex = 4;
+            this.minutesLabel.Text = "Minutes";
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursLabel.Location = new System.Drawing.Point(204, 151);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(65, 27);
+            this.hoursLabel.TabIndex = 6;
+            this.hoursLabel.Text = "Hours";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.Transparent;
+            this.startButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(12, 267);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(150, 39);
+            this.startButton.TabIndex = 8;
+            this.startButton.Text = "Start (F6)";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.BackColor = System.Drawing.Color.Transparent;
+            this.stopButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stopButton.Location = new System.Drawing.Point(222, 267);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(150, 39);
+            this.stopButton.TabIndex = 9;
+            this.stopButton.Text = "Stop (F6)";
+            this.stopButton.UseVisualStyleBackColor = false;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Transparent;
+            this.resetButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(222, 325);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(150, 39);
+            this.resetButton.TabIndex = 10;
+            this.resetButton.Text = "Reset (F8)";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.countdownLabel.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.Location = new System.Drawing.Point(104, 203);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(168, 45);
+            this.countdownLabel.TabIndex = 11;
+            this.countdownLabel.Text = "00:00:00";
+            this.countdownLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondsUpDown
+            // 
+            this.secondsUpDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsUpDown.Location = new System.Drawing.Point(104, 80);
+            this.secondsUpDown.Name = "secondsUpDown";
+            this.secondsUpDown.Size = new System.Drawing.Size(94, 30);
+            this.secondsUpDown.TabIndex = 12;
+            // 
+            // minutesUpDown
+            // 
+            this.minutesUpDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesUpDown.Location = new System.Drawing.Point(104, 116);
+            this.minutesUpDown.Name = "minutesUpDown";
+            this.minutesUpDown.Size = new System.Drawing.Size(94, 30);
+            this.minutesUpDown.TabIndex = 13;
+            // 
+            // hoursUpDown
+            // 
+            this.hoursUpDown.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursUpDown.Location = new System.Drawing.Point(104, 152);
+            this.hoursUpDown.Name = "hoursUpDown";
+            this.hoursUpDown.Size = new System.Drawing.Size(94, 30);
+            this.hoursUpDown.TabIndex = 14;
+            // 
+            // countdownTimer
+            // 
+            this.countdownTimer.Interval = 1000;
+            this.countdownTimer.Tick += new System.EventHandler(this.countdownTimer_Tick);
+            // 
+            // restartButton
+            // 
+            this.restartButton.BackColor = System.Drawing.Color.Transparent;
+            this.restartButton.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.restartButton.Location = new System.Drawing.Point(12, 325);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(150, 39);
+            this.restartButton.TabIndex = 15;
+            this.restartButton.Text = "Restart (F7)";
+            this.restartButton.UseVisualStyleBackColor = false;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EggTimer.Properties.Resources.egg_background;
+            this.ClientSize = new System.Drawing.Size(384, 376);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.hoursUpDown);
+            this.Controls.Add(this.minutesUpDown);
+            this.Controls.Add(this.secondsUpDown);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.hoursLabel);
+            this.Controls.Add(this.minutesLabel);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Egg Timer";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AutoClickerFormClose);
+            this.Load += new System.EventHandler(this.AutoClickerFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.secondsUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minutesUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label minutesLabel;
+        private System.Windows.Forms.Label hoursLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label countdownLabel;
+        private System.Windows.Forms.NumericUpDown secondsUpDown;
+        private System.Windows.Forms.NumericUpDown minutesUpDown;
+        private System.Windows.Forms.NumericUpDown hoursUpDown;
+        private System.Windows.Forms.Timer countdownTimer;
+        private System.Windows.Forms.Button restartButton;
+    }
+}
+
---
+++ EggTimer/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace EggTimer
+{
+    public partial class mainForm : Form
+    {
+        private TimeSpan timeSpan;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int RegisterHotKey(IntPtr hWnd, int id, uint modifier, uint vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        //ID for the F6, F7 Hotkey, F8 hotkey used later to check which hotkey is being used
+        private const int F6HOTKEYID = 1;
+        private const int F7HOTKEYID = 2;
+        private const int F8HOTKEYID = 3;
+
+        //Constant value for the hotkey value using WndProc
+        private const int WM_HOTKEY = 0x0312;
+
+        private Boolean reset = true;
+
+        private Boolean running = false;
+
+        //Modifiers for Hotkey registry
+        private enum Modifiers
+        {
+            NONE = 0x0000,
+            ALT = 0x0001,
+            CONTROL = 0x0002,
+            SHIFT = 0x0004,
+            WINDOWS = 0x0008
+        }
+
+        [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+        protected override void WndProc(ref Message m)
+        {
+            //Check for key down press
+            if (m.Msg == WM_HOTKEY)
+            {
+                //Switch over the ID assigned
+                switch (m.WParam.ToInt32())
+                {
+                    case F6HOTKEYID:
+                        if (running)
+                        {
+                            stopButton.PerformClick();
+                        }
+                        else if (!running)
+                        {
+                            startButton.PerformClick();
+                        }
+                        break;
+
+                    case F7HOTKEYID:
+                        restartButton.PerformClick();
+                        break;
+
+                    case F8HOTKEYID:
+                        resetButton.PerformClick();
+                        break;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        //Method to run when the Auto Clicker window loads
+        private void AutoClickerFormLoad(object sender, EventArgs e)
+        {
+            //Register the F6 hotkey with the system
+            RegisterHotKey(this.Handle, F6HOTKEYID, (uint)Modifiers.NONE, (uint)Keys.F6);
+
+            //Register the F7 hotkey with the system
+            RegisterHotKey(this.Handle, F7HOTKEYID, (uint)Modifiers.NONE, (uint)Keys.F7);
+
+            //Register the F8 hotkey with the system
+            RegisterHotKey(this.Handle, F8HOTKEYID, (uint)Modifiers.NONE, (uint)Keys.F8);
+        }
+
+        //Method to run when the Auto Clicker window closes
+        private void AutoClickerFormClose(object sender, FormClosedEventArgs e)
+        {
+            //Unregister the F6 hotkey with the system
+            UnregisterHotKey(this.Handle, F6HOTKEYID);
+
+            //Unregister the F7 hotkey with the system
+            UnregisterHotKey(this.Handle, F7HOTKEYID);
+
+            //Unregister the F7 hotkey with the system
+            UnregisterHotKey(this.Handle, F8HOTKEYID);
+        }
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+            //Set all the labels to transparent backgrounds
+            titleLabel.BackColor = Color.Transparent;
+            hoursLabel.BackColor = Color.Transparent;
+            minutesLabel.BackColor = Color.Transparent;
+            secondsLabel.BackColor = Color.Transparent;
+
+            //Remove button borders
+            startButton.FlatStyle = FlatStyle.Flat;
+            startButton.FlatAppearance.BorderSize = 0;
+
+            resetButton.FlatStyle = FlatStyle.Flat;
+            resetButton.FlatAppearance.BorderSize = 0;
+
+            stopButton.FlatStyle = FlatStyle.Flat;
+            stopButton.FlatAppearance.BorderSize = 0;
+
+            restartButton.FlatStyle = FlatStyle.Flat;
+            restartButton.FlatAppearance.BorderSize = 0;
+        }
+
+        private void startButton_Click(Object sender, EventArgs e)
+        {
+            double seconds = (double)secondsUpDown.Value;
+            double minutes = (double)minutesUpDown.Value;
+            double hours = (double)hoursUpDown.Value;
+
+            double totalSeconds = seconds + (minutes * 60) + (hours * 3600);
+
+            if (reset && !running && totalSeconds > 0)
+            {
+
+                timeSpan = TimeSpan.FromSeconds(totalSeconds);
+                countdownTimer.Start();
+
+                new ToastContentBuilder()
+                    .AddText("Timer Started!")
+                    .Show();
+
+                reset = false;
+
+                running = true;
+            }
+            else if (!reset && !running)
+            {
+                countdownTimer.Start();
+
+                reset = false;
+
+                running = true;
+            }
+        }
+
+        private void restartButton_Click(Object sender, EventArgs e)
+        {
+            if (running)
+            {
+                countdownTimer.Stop();
+                running = false;
+            }
+
+            double seconds = (double)secondsUpDown.Value;
+            double minutes = (double)minutesUpDown.Value;
+            double hours = (double)hoursUpDown.Value;
+            double totalSeconds = seconds + (minutes * 60) + (hours * 3600);
+
+            timeSpan = TimeSpan.FromSeconds(totalSeconds);
+            countdownTimer.Start();
+
+            new ToastContentBuilder()
+                .AddText("Timer Restarted!")
+                .Show();
+
+            reset = false;
+
+            running = true;
+        }
+
+        private void stopButton_Click(Object sender, EventArgs e)
+        {
+            running = false;
+
+            countdownTimer.Stop();
+        }
+
+        private void resetButton_Click(Object sender, EventArgs e)
+        {
+            if (running)
+            {
+                countdownTimer.Stop();
+                running = false;
+            }
+
+            secondsUpDown.Value = 0;
+            minutesUpDown.Value = 0;
+            hoursUpDown.Value = 0;
+
+            timeSpan = TimeSpan.Zero;
+            countdownLabel.Text = timeSpan.ToString(@"hh\:mm\:ss");
+
+            reset = true;
+        }
+
+        private void countdownTimer_Tick(Object sender, EventArgs e)
+        {
+            if (timeSpan.TotalSeconds == 0)
+            {
+                countdownTimer.Stop();
+
+                running = false;
+                reset = true;
+
+                new ToastContentBuilder()
+                    .AddText("Timer Complete!")
+                    .Show();
+            }
+            else
+            {
+                timeSpan = timeSpan.Subtract(TimeSpan.FromSeconds(1));
+                countdownLabel.Text = timeSpan.ToString(@"hh\:mm\:ss");
+            }
+        }
+    }
+}
